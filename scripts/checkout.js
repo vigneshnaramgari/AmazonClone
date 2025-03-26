ff8bac283bcdd9af2298ca94b9ea74594c549e53
Vignesh <nsaivignesh2005@gmail.com>
2025-03-26 12:01:04 +0530

    Added new

---
+++ scripts/checkout.js
@@ -0,0 +1,27 @@
+import {renderOrderSummary} from './checkout/orderSummary.js';
+import {renderPaymentSummary} from './checkout/paymentSummary.js';
+import {loadProducts, loadProductsFetch} from '../data/products.js';
+import {loadCart} from '../data/cart.js';
+
+
+async function loadPage() {
+  try {
+      
+    await loadProductsFetch();
+
+    const value = await new Promise((resolve, reject) => {
+
+      loadCart(() => {
+
+        resolve('value3');
+      });
+    });
+
+  } catch (error) {
+    console.log('Unexpected error. Please try again later.');
+  }
+
+  renderOrderSummary();
+  renderPaymentSummary();
+}
+loadPage();
